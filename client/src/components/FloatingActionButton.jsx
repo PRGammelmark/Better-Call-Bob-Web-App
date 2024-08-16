d00c9c0deb6117aab8a25f22190b20c8fa6d01d1
Patrick Gammelmark <patrickroeikjaer@gmail.com>
2024-08-16 10:31:35 +0200

    Update floating action button to ref createUser

--- client/src/components/FloatingActionButton.jsx
+++ client/src/components/FloatingActionButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import FloatingActionButtonCSS from "./FloatingActionButton.module.css"
 import AddNewIcon from "../assets/add-new.svg"
-import AddTaskIcon from "../assets/add-task.svg"
+import AddNewTaskIcon from "../assets/add-task.svg"
 import { useState } from 'react'
 import { useNavigate } from "react-router-dom"
 
@@ -19,13 +19,23 @@ const FloatingActionButton = () => {
         navigate("ny-opgave")
     }
 
+    function redirectCreateUser() {
+        setMenuOpen(false)
+        navigate("ny-bruger")
+    }
+
   return (
-    <div className={FloatingActionButtonCSS.floatingActionButton} onClick={toggleMenu}>
-        <img src={AddNewIcon} draggable="false" alt="" className={FloatingActionButtonCSS.addNewIcon} style={{transform: menuOpen ? "rotate(0deg) scale(0.8)" : "rotate(135deg) scale(1)"}}/>
-        <div className={FloatingActionButtonCSS.addTaskButton} onClick={redirectCreateTask} style={{transform: menuOpen ? "translateY(-100px) scale(1)" : "translateY(0px) scale(0)", opacity: menuOpen ? "1" : "0"}}>
-            <img src={AddTaskIcon} className={FloatingActionButtonCSS.addNewTaskIcon} alt="" />
+    <>
+        <div className={FloatingActionButtonCSS.floatingActionButton} onClick={toggleMenu}>
+            <img src={AddNewIcon} draggable="false" alt="" className={FloatingActionButtonCSS.addNewIcon} style={{transform: menuOpen ? "rotate(0deg) scale(0.8)" : "rotate(135deg) scale(1)"}}/>
         </div>
-    </div>
+        <div className={FloatingActionButtonCSS.addNewTaskButton} onClick={redirectCreateTask} style={{transform: menuOpen ? "translateY(-100px) scale(1)" : "translateY(0px) scale(0)", opacity: menuOpen ? "1" : "0"}}>
+            <span className={FloatingActionButtonCSS.icons}>📋</span>
+        </div>
+        <div className={FloatingActionButtonCSS.addNewUserButton} onClick={redirectCreateUser} style={{transform: menuOpen ? "translateX(-100px) scale(1)" : "translate(0px) scale(0)", opacity: menuOpen ? "1" : "0"}}>
+            <span className={FloatingActionButtonCSS.icons}>👷🏼‍♂️</span>
+        </div>
+    </>
   )
 }
 
