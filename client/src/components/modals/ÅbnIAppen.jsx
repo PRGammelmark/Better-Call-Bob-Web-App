e6eaaa51e48b4aaef92e81f99b41f6e2e4ac99c9
Patrick Gammelmark <patrickroeikjaer@gmail.com>
2025-01-31 11:01:49 +0100

    Update invitation to install app

--- client/src/components/modals/ÅbnIAppen.jsx
+++ client/src/components/modals/ÅbnIAppen.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import Modal from '../Modal.jsx'
 import Styles from './ÅbnIAppen.module.css'
+import DelSafariIcon from '../../assets/delSafariIcon.png'
+import SafariIcon from '../../assets/safariIcon.png'
+import InstallerSafariIcon from '../../assets/installerSafariIcon.png'
+import AppIcon from '../../../public/apple-icon-180.png'
 
 const ÅbnIAppen = (props) => {
     const [deferredPrompt, setDeferredPrompt] = useState(null);
+    const isIos = /iphone|ipad|ipod/i.test(navigator.userAgent);
 
     useEffect(() => {
         const handler = (event) => {
-            console.log("beforeinstallprompt event fired")
             event.preventDefault();
             setDeferredPrompt(event);
         };
@@ -42,9 +46,21 @@ const ÅbnIAppen = (props) => {
         <Modal trigger={props.trigger} setTrigger={props.setTrigger} >
             <h2 className={Styles.modalHeading}>Du får en bedre oplevelse i app'en!</h2>
             <p className={Styles.modalText}>Bruger du din telefon til at tilgå Better Call Bob? Brug et par sekunder på at installere app'en – så får du en bedre oplevelse.</p>
-            <button className={Styles.modalButton} onClick={installApp} disabled={!deferredPrompt}>
-                Installer app
-            </button>
+            {isIos 
+                ? 
+                <div className={Styles.installInstructions}>
+                    <h3>Sådan installerer du app'en:</h3>
+                    <ol>
+                        <li className={Styles.installStep}>1. Åbn denne side i Safari<img src={SafariIcon} alt="Safari icon" /></li>
+                        <li className={Styles.installStep}>2. Tryk på "Del"-knappen ...<img src={DelSafariIcon} alt="Del Safari icon" /></li>
+                        <li className={Styles.installStep}>3. Tryk på "Føj til hjemmeskærm"<img src={InstallerSafariIcon} alt="Installere icon" /></li>
+                        <li className={Styles.installStep}>4. App'en er nu installeret!<img src={AppIcon} alt="Installere icon" /></li>
+                    </ol>
+                </div>
+                : 
+                <button className={Styles.modalButton} onClick={installApp} disabled={!deferredPrompt}>
+                    Installer app
+                </button>}
         </Modal>
     );
 };
