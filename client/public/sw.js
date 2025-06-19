f95e35c946d1e9dbf65096461107acb6ed2d2e2c
Patrick Gammelmark <patrickroeikjaer@gmail.com>
2025-06-19 13:43:49 +0200

    Update sw

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,4 +1,4 @@
-console.log('Service Worker loaded');
+console.log('Service Worker loading ...');
 
 const staticCacheName = "site-static";
 const assets = [
@@ -8,6 +8,66 @@ const assets = [
     '/logo192.png',
     '/logo512.png'
 ];
+
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
+// A list of local resources we always want to be cached.
+const PRECACHE_URLS = [
+  '/',
+  '/index.html',
+  '/site.webmanifest',
+  '/logo192.png',
+  '/logo512.png'
+];
+
+// The install handler takes care of precaching the resources we always need.
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  );
+});
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', event => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
+
+// The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
+self.addEventListener('fetch', event => {
+  // Skip cross-origin requests, like those for Google Analytics.
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
+});
 
 // // install the service worker
 // self.addEventListener('install', (event) => {
@@ -33,29 +93,29 @@ const assets = [
 //   });
   
 
-self.addEventListener('install', event => {
-  console.log('→ install');
-  event.waitUntil(
-    caches.open(staticCacheName).then(cache => cache.addAll(assets))
-      .then(self.skipWaiting())
-  );
-});
+// self.addEventListener('install', event => {
+//   console.log('→ install');
+//   event.waitUntil(
+//     caches.open(staticCacheName).then(cache => cache.addAll(assets))
+//       .then(self.skipWaiting())
+//   );
+// });
 
-self.addEventListener('activate', event => {
-  console.log('→ activate');
-  event.waitUntil(self.clients.claim());
-});
+// self.addEventListener('activate', event => {
+//   console.log('→ activate');
+//   event.waitUntil(self.clients.claim());
+// });
 
 
-// fetch the service worker
-self.addEventListener('fetch', (event) => {
-    console.log('Service Worker fetching');
-    event.respondWith(
-        caches.match(event.request).then((cacheResponse) => {
-            return cacheResponse || fetch(event.request);
-        })
-    );
-});
+// // fetch the service worker
+// self.addEventListener('fetch', (event) => {
+//     console.log('Service Worker fetching');
+//     event.respondWith(
+//         caches.match(event.request).then((cacheResponse) => {
+//             return cacheResponse || fetch(event.request);
+//         })
+//     );
+// });
 
 // PUSH NOTIFICATIONS
 
