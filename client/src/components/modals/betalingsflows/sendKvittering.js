501dadba7af4b27e880906a1157872540c939a93
Patrick Gammelmark <patrickroeikjaer@gmail.com>
2026-01-27 23:30:03 +0100

    Hotfix

--- client/src/components/modals/betalingsflows/sendKvittering.js
+++ client/src/components/modals/betalingsflows/sendKvittering.js
@@ -2,6 +2,145 @@ import * as beregn from '../../../utils/beregninger.js'
 import dayjs from 'dayjs'
 import axios from 'axios'
 
+// ===== HJÆLPEFUNKTIONER =====
+
+/**
+ * Formaterer et beløb til dansk valuta
+ */
+function formaterBeløb(beløb, decimaler = 2) {
+    return beløb.toLocaleString('da-DK', {
+        style: 'currency',
+        currency: 'DKK',
+        minimumFractionDigits: decimaler,
+        maximumFractionDigits: decimaler
+    });
+}
+
+/**
+ * Aggregerer timeregistreringer fra alle posteringer
+ * Grupperer på timetypeId og summerer antal og pris
+ */
+function aggregerTimeregistreringer(posteringer) {
+    const map = new Map();
+    
+    posteringer.forEach(postering => {
+        (postering.timeregistrering || []).forEach(tr => {
+            const key = tr.timetypeId?.toString() || tr.navn || 'unknown';
+            if (map.has(key)) {
+                const existing = map.get(key);
+                existing.antal += tr.antal || 0;
+                existing.totalInklMoms += tr.pris?.totalInklMoms || 0;
+            } else {
+                map.set(key, {
+                    navn: tr.navn || 'Timer',
+                    antal: tr.antal || 0,
+                    totalInklMoms: tr.pris?.totalInklMoms || 0
+                });
+            }
+        });
+    });
+    
+    return Array.from(map.values());
+}
+
+/**
+ * Aggregerer faste tillæg fra alle posteringer
+ * Grupperer på tillaegId og summerer antal og pris
+ */
+function aggregerFasteTillæg(posteringer) {
+    const map = new Map();
+    
+    posteringer.forEach(postering => {
+        (postering.fasteTillæg || []).forEach(ft => {
+            const key = ft.tillaegId?.toString() || ft.navn || 'unknown';
+            if (map.has(key)) {
+                const existing = map.get(key);
+                existing.antal += ft.antal || 0;
+                existing.totalInklMoms += ft.pris?.totalInklMoms || 0;
+            } else {
+                map.set(key, {
+                    navn: ft.navn || 'Tillæg',
+                    antal: ft.antal || 0,
+                    totalInklMoms: ft.pris?.totalInklMoms || 0
+                });
+            }
+        });
+    });
+    
+    return Array.from(map.values());
+}
+
+/**
+ * Aggregerer procent tillæg fra alle posteringer
+ * Grupperer på procentTillaegId og summerer pris
+ */
+function aggregerProcentTillæg(posteringer) {
+    const map = new Map();
+    
+    posteringer.forEach(postering => {
+        (postering.procentTillæg || []).forEach(pt => {
+            const key = pt.procentTillaegId?.toString() || pt.navn || 'unknown';
+            if (map.has(key)) {
+                const existing = map.get(key);
+                existing.antal += 1;
+                existing.totalInklMoms += pt.pris?.totalInklMoms || 0;
+            } else {
+                map.set(key, {
+                    navn: pt.navn || 'Procent tillæg',
+                    antal: 1,
+                    totalInklMoms: pt.pris?.totalInklMoms || 0
+                });
+            }
+        });
+    });
+    
+    return Array.from(map.values());
+}
+
+/**
+ * Samler alle materialer fra alle posteringer (uden aggregering)
+ */
+function samlMaterialer(posteringer) {
+    const materialer = [];
+    
+    posteringer.forEach(postering => {
+        (postering.materialer || []).forEach(m => {
+            materialer.push({
+                beskrivelse: m.beskrivelse || m.varenummer || 'Materiale',
+                antal: m.antal || 1,
+                totalInklMoms: m.totalInklMoms || 0,
+                erUdlaeg: m.erUdlaeg || false
+            });
+        });
+    });
+    
+    return materialer;
+}
+
+/**
+ * Beregner total pris inkl. moms fra alle posteringer
+ */
+function beregnTotalInklMoms(posteringer) {
+    return posteringer.reduce((sum, p) => sum + (p.totalPrisInklMoms || 0), 0);
+}
+
+/**
+ * Beregner total moms fra alle posteringer
+ */
+function beregnTotalMoms(posteringer) {
+    return posteringer.reduce((sum, p) => sum + (p.totalMoms || 0), 0);
+}
+
+/**
+ * Beregner total betalinger fra alle posteringer
+ */
+function beregnTotalBetalinger(posteringer) {
+    return posteringer.reduce((sum, p) => {
+        const betalingerSum = (p.betalinger || []).reduce((bSum, b) => bSum + (b.betalingsbeløb || 0), 0);
+        return sum + betalingerSum;
+    }, 0);
+}
+
 function sendKvittering({posteringer, user, kunde, opgave, setErrorMessage}) {
 
     console.log("Kvittering sendes.")
@@ -42,153 +181,116 @@ function sendKvittering({posteringer, user, kunde, opgave, setErrorMessage}) {
         ansvarligeMedarbejdere = [...new Set(posteringer.map(postering => postering.bruger.navn))].join(', ');
     }
 
-    // ===== BEREGNING AF KVITTERINGSINDHOLD =====
-    const antalOpstartsgebyrer = beregn.antalOpstartsgebyrerForPris(posteringer)
-    const antalHandymanTimer = beregn.antalHandymanTimerForPris(posteringer)
-    const antalTømrerTimer = beregn.antalTømrerTimerForPris(posteringer)
-    const antalRådgivningOpmålingVejledning = beregn.antalRådgivningOpmålingVejledningForPris(posteringer)
-    const antalTrailer = beregn.antalTrailerForPris(posteringer)
-    const antalAftenTillæg = beregn.antalAftenTillægForPris(posteringer)
-    const antalNatTillæg = beregn.antalNatTillægForPris(posteringer)
-    const antalUdlæg = beregn.antalUdlæg(posteringer)
-
     const isEnglish = kunde?.isEnglish || false;
 
-    // ===== OPSÆTNING AF KVITTERINGSLINJER =====
-    const opstartsgebyrLinje = antalOpstartsgebyrer > 0 ? `
+    // ===== AGGREGER DATA FRA NYE FELTER =====
+    const timeregistreringer = aggregerTimeregistreringer(posteringer);
+    const fasteTillæg = aggregerFasteTillæg(posteringer);
+    const procentTillæg = aggregerProcentTillæg(posteringer);
+    const materialer = samlMaterialer(posteringer);
+    
+    // ===== BEREGN TOTALER =====
+    const totalInklMoms = beregnTotalInklMoms(posteringer);
+    const totalMoms = beregnTotalMoms(posteringer);
+    const totalBetalinger = beregnTotalBetalinger(posteringer);
+
+    // ===== GENERER KVITTERINGSLINJER =====
+    
+    // Timeregistreringer (timer)
+    const timeregistreringLinjer = timeregistreringer
+        .filter(tr => tr.totalInklMoms > 0)
+        .map(tr => `
         <tr>
-            <td>${antalOpstartsgebyrer} x ${isEnglish ? "start-up fees" : "opstartsgebyr(er)"}:</td>
-            <td style="text-align: right;">${beregn.opstartPris(posteringer, 2, true).formateret}</td>
-        </tr>` : '';
+            <td>${tr.antal} x ${tr.navn}:</td>
+            <td style="text-align: right;">${formaterBeløb(tr.totalInklMoms)}</td>
+        </tr>`).join('');
 
-    const handymanTimerLinje = antalHandymanTimer > 0 ? `
+    // Faste tillæg (opstart, trailer, etc.)
+    const fasteTillægLinjer = fasteTillæg
+        .filter(ft => ft.totalInklMoms > 0)
+        .map(ft => `
         <tr>
-            <td>${antalHandymanTimer} x ${isEnglish ? "handyman hours" : "handymantimer"}:</td>
-            <td style="text-align: right;">${beregn.handymanPris(posteringer, 2, true).formateret}</td>
-        </tr>` : '';
+            <td>${ft.antal} x ${ft.navn}:</td>
+            <td style="text-align: right;">${formaterBeløb(ft.totalInklMoms)}</td>
+        </tr>`).join('');
 
-    const tømrerTimerLinje = antalTømrerTimer > 0 ? `
+    // Procent tillæg (aften, nat, etc.)
+    const procentTillægLinjer = procentTillæg
+        .filter(pt => pt.totalInklMoms > 0)
+        .map(pt => `
         <tr>
-            <td>${antalTømrerTimer} x ${isEnglish ? "carpenter hours" : "tømrertimer"}:</td>
-            <td style="text-align: right;">${beregn.tømrerPris(posteringer, 2, true).formateret}</td>
-        </tr>` : '';
+            <td>${pt.antal > 1 ? pt.antal + ' x ' : ''}${pt.navn}:</td>
+            <td style="text-align: right;">${formaterBeløb(pt.totalInklMoms)}</td>
+        </tr>`).join('');
 
-    const rådgivningOpmålingVejledningLinje = antalRådgivningOpmålingVejledning > 0 ? `
+    // Materialer samlet linje + detaljer
+    const materialerTotal = materialer.reduce((sum, m) => sum + m.totalInklMoms, 0);
+    const materialerLinje = materialerTotal > 0 ? `
         <tr>
-            <td>${antalRådgivningOpmålingVejledning} x ${isEnglish ? "counseling hours" : "rådgivnings- og vejledningstimer"}:</td>
-            <td style="text-align: right;">${beregn.rådgivningPris(posteringer, 2, true).formateret}</td>
-        </tr>` : '';
-
-    const aftenTillægLinje = antalAftenTillæg > 0 ? `
-        <tr>
-            <td>${antalAftenTillæg} x ${isEnglish ? "evening fees" : "aftentillæg"}:</td>
-            <td style="text-align: right;">${beregn.aftenTillægPris(posteringer, 2, true).formateret}</td>
-        </tr>` : '';
-
-    const natTillægLinje = antalNatTillæg > 0 ? `
-        <tr>
-            <td>${antalNatTillæg} x ${isEnglish ? "night fees" : "nattilæg"}:</td>
-            <td style="text-align: right;">${beregn.natTillægPris(posteringer, 2, true).formateret}</td>
-        </tr>` : '';
-
-    // const handymanTimerInklAftenTillægLinje = antalHandymanTimerAftenTillæg > 0 ? `
-    // <tr>
-    //     <td>${antalHandymanTimerAftenTillæg} ${isEnglish ? "handyman hours (plus evening fee)" : "handymantimer (inkl. aftentillæg)"}:</td>
-    //     <td style="text-align: right;">${(totalHandymanTimerAftenTillæg * 1.25).toLocaleString('da-DK', { style: 'currency', currency: 'DKK' })}</td>
-    // </tr>` : '';
-
-    // const tømrerTimerInklAftenTillægLinje = antalTømrerTimerAftenTillæg > 0 ? `
-    //     <tr>
-    //         <td>${antalTømrerTimerAftenTillæg} ${isEnglish ? "carpenter hours (plus evening fee)" : "tømrertimer (inkl. aftentillæg)"}:</td>
-    //         <td style="text-align: right;">${(totalTømrerTimerAftenTillæg * 1.25).toLocaleString('da-DK', { style: 'currency', currency: 'DKK' })}</td>
-    //     </tr>` : '';
-
-    // const rådgivningOpmålingVejledningInklAftenTillægLinje = antalRådgivningOpmålingVejledningAftenTillæg > 0 ? `
-    //     <tr>
-    //         <td>${antalRådgivningOpmålingVejledningAftenTillæg} ${isEnglish ? "counseling hours (plus evening fee)" : "rådgivnings- og vejledningstimer (inkl. aftentillæg)"}:</td>
-    //         <td style="text-align: right;">${(totalRådgivningOpmålingVejledningAftenTillæg * 1.25).toLocaleString('da-DK', { style: 'currency', currency: 'DKK' })}</td>
-    //     </tr>` : '';
-
-    // const handymanTimerInklNatTillægLinje = antalHandymanTimerNatTillæg > 0 ? `
-    // <tr>
-    //     <td>${antalHandymanTimerNatTillæg} ${isEnglish ? "handyman hours (plus night fee)" : "handymantimer (inkl. nattillæg)"}:</td>
-    //     <td style="text-align: right;">${(totalHandymanTimerNatTillæg * 1.25).toLocaleString('da-DK', { style: 'currency', currency: 'DKK' })}</td>
-    // </tr>` : '';
-
-    // const tømrerTimerInklNatTillægLinje = antalTømrerTimerNatTillæg > 0 ? `
-    //     <tr>
-    //         <td>${antalTømrerTimerNatTillæg} ${isEnglish ? "carpenter hours (plus night fee)" : "tømrertimer (inkl. nattillæg)"}:</td>
-    //         <td style="text-align: right;">${(totalTømrerTimerNatTillæg * 1.25).toLocaleString('da-DK', { style: 'currency', currency: 'DKK' })}</td>
-    //     </tr>` : '';
-
-    // const rådgivningOpmålingVejledningInklNatTillægLinje = antalRådgivningOpmålingVejledningNatTillæg > 0 ? `
-    //     <tr>
-    //         <td>${antalRådgivningOpmålingVejledningNatTillæg} ${isEnglish ? "counseling hours (plus night fee)" : "rådgivnings- og vejledningstimer (inkl. nattillæg)"}:</td>
-    //         <td style="text-align: right;">${(totalRådgivningOpmålingVejledningNatTillæg * 1.25).toLocaleString('da-DK', { style: 'currency', currency: 'DKK' })}</td>
-    //     </tr>` : '';
-
-    const trailerLinje = antalTrailer > 0 ? `
-        <tr>
-            <td>${antalTrailer} x ${isEnglish ? "trailer use" : "trailerbrug"}:</td>
-            <td style="text-align: right;">${beregn.trailerPris(posteringer, 2, true).formateret}</td>
-        </tr>` : '';
-
-    const rabatLinje = beregn.rabatPris(posteringer).beløb > 0 ? `
-        <tr>
-            <td>${isEnglish ? "Discount" : "Rabat"}:</td>
-            <td style="text-align: right;">-${beregn.rabatPris(posteringer, 2, true).formateret}</td>
-        </tr>` : '';
-
-    const fastPrisLinje = beregn.fastPris(posteringer).beløb > 0 ? `
-        <tr>
-            <td>${isEnglish ? "Fixed price" : "Fast pris"}:</td>
-            <td style="text-align: right;">${beregn.fastPris(posteringer, 2, true).formateret}</td>
-        </tr>` : '';
-
-    const materialerLinje = antalUdlæg > 0 ? `
-        <tr>
-            <td>${isEnglish ? "Total outlays and materials" : "Udlæg & materialer, i alt"}:</td>
-            <td style="text-align: right;">${beregn.udlægPris(posteringer, 2, true).formateret}</td>
+            <td>${isEnglish ? "Materials" : "Materialer"}, i alt:</td>
+            <td style="text-align: right;">${formaterBeløb(materialerTotal)}</td>
         </tr>` : '';
     
-    const materialeudlægDetaljer = posteringer.map(postering => postering.udlæg.map(udlæg => `
+    const materialeDetaljer = materialer
+        .filter(m => m.totalInklMoms > 0)
+        .map(m => `
         <tr>
-            <td style="font-size: 0.8rem; opacity: 0.5;">- ${udlæg.beskrivelse}:</td>
-            <td style="text-align: right; font-size: 0.8rem; opacity: 0.5;">${(udlæg.beløb * 1.25).toLocaleString('da-DK', { style: 'currency', currency: 'DKK' })}</td>
-        </tr>`).join('')).join('');
+            <td style="font-size: 0.8rem; opacity: 0.5;">- ${m.antal > 1 ? m.antal + ' x ' : ''}${m.beskrivelse}:</td>
+            <td style="text-align: right; font-size: 0.8rem; opacity: 0.5;">${formaterBeløb(m.totalInklMoms)}</td>
+        </tr>`).join('');
 
+    // Rabat linje (beregnet fra rabatBeløb i arrays)
+    const rabatBeløb = beregn.rabatPris(posteringer, 2, true)?.beløb || 0;
+    const rabatLinje = rabatBeløb > 0 ? `
+        <tr>
+            <td>${isEnglish ? "Discount" : "Rabat"}:</td>
+            <td style="text-align: right;">-${formaterBeløb(rabatBeløb)}</td>
+        </tr>` : '';
+
+    // Fast pris linje (tilbudspris)
+    const fastPrisBeløb = beregn.fastPris(posteringer, 2, true)?.beløb || 0;
+    const fastPrisLinje = fastPrisBeløb > 0 ? `
+        <tr>
+            <td>${isEnglish ? "Fixed price" : "Fast pris"}:</td>
+            <td style="text-align: right;">${formaterBeløb(fastPrisBeløb)}</td>
+        </tr>` : '';
+
+    // Total linje
     const totalLinje = `
         <tr style="border-top: 1px solid black; padding-top: 20px;">
             <td style="font-size: 1.3rem; padding-top: 10px;"><b>Total:</b></td>
-            <td style="text-align: right; font-size: 1.3rem; padding-top: 10px;"><b>${beregn.totalPris(posteringer, 2, true).formateret}</b></td>
+            <td style="text-align: right; font-size: 1.3rem; padding-top: 10px;"><b>${formaterBeløb(totalInklMoms)}</b></td>
         </tr>`;
 
+    // Moms linje
     const momsLinje = `
         <tr>
             <td style="opacity: 0.5;">${isEnglish ? "VAT" : "Heraf moms"}:</td>
-            <td style="text-align: right; opacity: 0.5;">${(beregn.totalPris(posteringer, 2, true).beløb * 0.2).toFixed(2)} kr.</td>
+            <td style="text-align: right; opacity: 0.5;">${formaterBeløb(totalMoms)}</td>
         </tr>`;
 
+    // Table headings
     const tableHeadings = `
         <tr style="font-size: 1.3rem; border: 1px solid white; padding-bottom: 20px;">
             <th style="text-align: left;"><b>${isEnglish ? "Description" : "Beskrivelse"}</b></th>
             <th style="text-align: right;"><b>${isEnglish ? "Amount" : "Beløb"}</b></th>
         </tr>`;
 
-    const betalingerDetaljer = posteringer.map(postering => postering.betalinger.map(betaling => `
+    // Betalinger detaljer
+    const betalingerDetaljer = posteringer.map(postering => (postering.betalinger || []).map(betaling => `
         <tr>
             <td style="font-size: 0.8rem; opacity: 0.5;">- ${betaling.betalingsmetode + " " + dayjs(betaling.dato).format('DD. MMMM YYYY')}:</td>
-            <td style="text-align: right; font-size: 0.8rem; opacity: 0.5;">${(betaling.betalingsbeløb).toLocaleString('da-DK', { style: 'currency', currency: 'DKK', minimumFractionDigits: 2, maximumFractionDigits: 2 })}</td>
+            <td style="text-align: right; font-size: 0.8rem; opacity: 0.5;">${formaterBeløb(betaling.betalingsbeløb)}</td>
         </tr>`).join('')).join('');
 
+    // Betalinger total linje
     const betalingerLinje = `
     <tr style="border-top: 1px solid black; padding-top: 20px;">
-        <td style="font-size: 1.3rem; padding-top: 10px;"><b>Betalt, i alt:</b></td>
-        <td style="text-align: right; font-size: 1.3rem; padding-top: 10px;"><b>${beregn.totalBetalinger(posteringer, 2).formateret}</b></td>
+        <td style="font-size: 1.3rem; padding-top: 10px;"><b>${isEnglish ? "Paid, total" : "Betalt, i alt"}:</b></td>
+        <td style="text-align: right; font-size: 1.3rem; padding-top: 10px;"><b>${formaterBeløb(totalBetalinger)}</b></td>
     </tr>`;
 
-    // <img src="https://bettercallbob.dk/wp-content/uploads/2024/01/Better-Call-Bob-logo-v2-1.svg" alt="BCB Logo via hjemmeside" style="width: 150px; height: auto; margin-top: -20px; display: flex; justify-content: flex-end;" />
-
+    // ===== SAML KVITTERING =====
     const kvittering = `
         <div style="background-color: #fff; color: #000; padding: 20px; border-radius: 10px; display: inline-block; box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;">
             <img src="https://bettercallbob.dk/wp-content/uploads/2025/05/bcb-logo.png" alt="BCB Logo via hjemmeside" style="width: 150px; height: auto; margin-top: -20px;" />
@@ -200,17 +302,13 @@ function sendKvittering({posteringer, user, kunde, opgave, setErrorMessage}) {
             <hr style="border: 1px solid black; margin: 20px 0;" />
             <table style="width: 250px;">
                 ${tableHeadings}
-                ${opstartsgebyrLinje}
-                ${handymanTimerLinje}
-                ${tømrerTimerLinje}
-                ${rådgivningOpmålingVejledningLinje}
-                ${aftenTillægLinje}
-                ${natTillægLinje}
-                ${trailerLinje}
+                ${timeregistreringLinjer}
+                ${fasteTillægLinjer}
+                ${procentTillægLinjer}
                 ${rabatLinje}
                 ${fastPrisLinje}
                 ${materialerLinje}
-                ${materialeudlægDetaljer}
+                ${materialeDetaljer}
                 ${totalLinje}
                 ${momsLinje}
                 ${betalingerLinje}
